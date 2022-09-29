d40ace59b79bd4d2532d5bae651d08b56dc3679f
AlmirMenezesJr <jrjmswii@gmail.com>
2022-09-29 00:07:47 -0300

    first commit

---
+++ src/primeiro_projeto/PrimeiraClasse.java
@@ -0,0 +1,10 @@
+package primeiro_projeto;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ola mundo!!");
+	}
+
+}
